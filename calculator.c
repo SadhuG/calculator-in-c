2a23e29167e4a0e50cf64def6f9b2231b92c30ba
Sadhu <96883913+SadhuG@users.noreply.github.com>
2024-09-27 15:36:49 +0530

    Basic Functionality Completed

---
+++ calculator.c
@@ -0,0 +1,96 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void clearScreen()
+{
+#ifdef _WIN32
+  system("cls");
+#else
+  system("clear");
+#endif
+}
+
+void pressAnyKeyToContinue()
+{
+  system("pause");
+}
+
+int main()
+{
+  // Variables
+  double firstNumber, secondNumber, result;
+  char operator, exitCall;
+
+  while (1)
+  {
+    // Clearing Old Console and Taking Inputs
+    clearScreen();
+
+    printf("Enter the first number: ");
+    scanf(" %lf", &firstNumber);
+
+    printf("Enter the operator (+ , - , * , / , %%) : ");
+    scanf(" %c", &operator);
+
+    printf("Enter the second number: ");
+    scanf(" %lf", &secondNumber);
+
+    switch (operator)
+    {
+    case '+':
+      result = firstNumber + secondNumber;
+      break;
+
+    case '-':
+      result = firstNumber - secondNumber;
+      break;
+
+    case '*':
+      result = firstNumber * secondNumber;
+      break;
+
+    case '/':
+      if (secondNumber == 0)
+      {
+        printf("Error: You cannot divide a number by Zero \n");
+        pressAnyKeyToContinue();
+        continue;
+      }
+      else
+      {
+        result = firstNumber / secondNumber;
+        break;
+      }
+
+    case '%':
+      if (secondNumber == 0)
+      {
+        printf("Error: You cannot divide a number by Zero \n");
+        pressAnyKeyToContinue();
+        continue;
+      }
+      else
+      {
+        result = (int)firstNumber % (int)secondNumber;
+        break;
+      }
+
+    default:
+      printf("Error invalid operator \n");
+      // pressAnyKeyToContinue();
+      getchar();
+      continue;
+    }
+
+    printf("The result is: %lf \n", result);
+
+    printf("Do you wanna calculate more...? /n type 'y'Yes or 'n'No: ");
+    scanf(" %c", &exitCall);
+
+    if (exitCall == 'n')
+    {
+      break;
+    }
+  }
+  return 0;
+}
